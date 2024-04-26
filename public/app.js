23a3370b43e15f031888528d38df4a2ac110f99b
0x890 <ninou123tu@gmail.com>
2024-04-26 02:46:04 +0100

    make it better

--- public/app.js
+++ public/app.js
@@ -22,6 +22,8 @@ $(document).ready(() => {
 })
 
 function connect() {
+    updateRoomStats();
+
     let uniqueId = window.settings.username || $('#uniqueIdInput').val();
     if (uniqueId !== '') {
 
@@ -60,8 +62,15 @@ function sanitize(text) {
 }
 
 function updateRoomStats() {
-    $('#roomStats').html(`Viewers: <b>${viewerCount.toLocaleString()}</b> Likes: <b>${likeCount.toLocaleString()}</b> Earned Diamonds: <b>${diamondsCount.toLocaleString()}</b>`)
+    $('#roomStats').html(`
+        <svg fill="#FFF" font-size="16px" viewBox="0 0 48 48" xmlns="http://www.w3.org/2000/svg" width="1em" height="1em"><path d="M5 14a10 10 0 1 1 20 0 10 10 0 0 1-20 0ZM15 27C5.35 27 0 34.04 0 39.84 0 43 1.76 43 7.94 43h14.12c6.18 0 7.94 0 7.94-3.16C30 34.04 24.65 27 15 27ZM33.98 42h7.2C45.7 42 47 42 47 39.63 47 35.28 43.08 30 36 30c-2 0-3.74.42-5.22 1.12A22.18 22.18 0 0 1 33.98 42ZM36 27a8 8 0 1 0 0-16 8 8 0 0 0 0 16Z"></path></svg> <b>${viewerCount.toLocaleString()}</b>
+        <svg fill="#FFF" font-size="16px" xmlns="http://www.w3.org/2000/svg" viewBox="0 -5 24 24" width="1.5em" height="1.5em">
+    <path d="M 15.9375 4 L 9.28125 9.03125 C 8.48425 9.63625 8.03125 10.59375 8.03125 11.59375 L 8 17 C 7.994 18.109 8.891 19 10 19 L 17.4375 19 C 18.0805 19 18.70775 18.56575 18.96875 17.84375 L 19.78125 13.59375 C 19.85525 13.38875 19.9015 12.1875 19.9375 11.0625 C 19.9755 9.9335 19.0665 9 17.9375 9 L 15.25 9 C 15.25 9 17.3125 6.16425 17.3125 5.65625 C 17.3125 4.34625 15.9375 4 15.9375 4 z M 4 10 L 4 19 L 7 19 L 7 10 L 4 10 z M 5.5 17 C 5.776 17 6 17.224 6 17.5 C 6 17.776 5.776 18 5.5 18 C 5.224 18 5 17.776 5 17.5 C 5 17.224 5.224 17 5.5 17 z"/>
+</svg> <b>${likeCount.toLocaleString()}</b>
+        <svg class="css-6gl0x9-StyledCoinIcon eux3x8p9" width="1em" data-e2e="" height="1em" viewBox="0 -5 48 48" fill="none" xmlns="http://www.w3.org/2000/svg"><circle cx="24" cy="24" r="22" fill="#FFEC9B"></circle><circle cx="24" cy="24" r="17" fill="#FACE15"></circle><path fill-rule="evenodd" clip-rule="evenodd" d="M40.9347 25.5C40.9779 25.0058 41 24.5055 41 24C41 14.6112 33.3888 7 24 7C14.6112 7 7 14.6112 7 24C7 24.5055 7.02206 25.0058 7.06527 25.5C7.82466 16.8137 15.1166 10 24 10C32.8834 10 40.1753 16.8137 40.9347 25.5Z" fill="#FABC15"></path><path d="M33 19C30.2041 19 27.9375 16.7614 27.9375 14H24.5625V27.6111C24.5625 29.2986 23.1774 30.6667 21.4688 30.6667C19.7601 30.6667 18.375 29.2986 18.375 27.6111C18.375 25.9236 19.7601 24.5556 21.4688 24.5556C21.722 24.5556 21.9659 24.5853 22.1981 24.6406C22.2365 24.6497 22.2747 24.6596 22.3125 24.6701V21.2763C22.0358 21.2406 21.7541 21.2222 21.4688 21.2222C17.8962 21.2222 15 24.0826 15 27.6111C15 31.1396 17.8962 34 21.4688 34C25.0413 34 27.9375 31.1396 27.9375 27.6111V20.6673C29.3477 21.7134 31.1005 22.3333 33 22.3333V19Z" fill="#FEF5CD"></path></svg> <b>${diamondsCount.toLocaleString()}</b>
+    `);
 }
+
 
 function generateUsernameLink(data) {
     console.log(data)
@@ -183,7 +192,7 @@ connection.on('member', (msg) => {
 
     setTimeout(() => {
         joinMsgDelay -= addDelay;
-        addChatItem('#21b2c2', msg, 'joined', true);
+        addChatItem('#21b2c2', msg, 'JOINED', true);
     }, joinMsgDelay);
 })
 
